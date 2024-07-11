703256074c43b80132f9bd1aade4dbb003fd5588
Din <rivojidin97@gmail.com>
2024-07-12 04:25:37 +0900

    feat: develop popular dishes sectional component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,21 +1,22 @@
 import React from "react";
-import Statistics from "./Statistics";
-import PopularDishes from "./PopularDishes";
-import NewDishes from "./NewDishes";
-import Advertisements from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
+import Advertisement from "./Advertisement";
 import Events from "./Events";
+import NewDishes from "./NewDishes";
+import PopularDishes from "./PopularDishes";
+import Statistics from "./Statistics";
 import "../../../css/home.css"
 
 
-export default function HomePage(){
-    return <div className="home-page">
-       <Statistics/>
-       <PopularDishes/>
-       <NewDishes/>
-       <Advertisements/>
-       <ActiveUsers/>
-       <Events/>
-
+export default function HomePage() {
+    return (<div className={"homepage"}>
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
+      
     </div>
+    );
 }
